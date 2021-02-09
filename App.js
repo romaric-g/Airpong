b57c9a30e62a5579ce74b6ed730dfa3c88249559
romaric-g <romaric.gauzi1@gmail.com>
2021-02-09 17:10:13 +0100

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { DeviceMotion, Gyroscope } from 'expo-sensors';
 
 export default function App() {
+  const [data, setData] = useState({
+    x: 0,
+    y: 0,
+    z: 0,
+  });
+  const [subscription, setSubscription] = useState(null);
+
+  const _slow = () => {
+    DeviceMotion.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    DeviceMotion.setUpdateInterval(16);
+  };
+
+  const _subscribe = () => {
+    setSubscription(
+      DeviceMotion.addListener(gyroscopeData => {
+        setData(gyroscopeData);
+        console.log("NEW DATA", gyroscopeData)
+      })
+    );
+  };
+
+  const _unsubscribe = () => {
+    subscription && subscription.remove();
+    setSubscription(null);
+  };
+
+  useEffect(() => {
+    _subscribe();
+    return () => _unsubscribe();
+  }, []);
+
+  const { x, y, z } = data;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Gyroscope:</Text>
+      <Text style={styles.text}>
+        x: {round(x)} y: {round(y)} z: {round(z)}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={subscription ? _unsubscribe : _subscribe} style={styles.button}>
+          <Text>{subscription ? 'On' : 'Off'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow} style={[styles.button, styles.middleButton]}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast} style={styles.button}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+function round(number) {
+  return Math.floor(Math.round(number))
+} 
 
 const styles = StyleSheet.create({
   container: {
