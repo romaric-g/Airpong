415cbefa456723f26ea8f10b3c5b748ef97ec594
romaric-g <romaric.gauzi1@gmail.com>
2021-02-12 00:31:41 +0100

    public server url

--- App/connection.tsx
+++ App/connection.tsx
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 
 //https://airpong.herokuapp.com/
 //http://localhost:4000
-const socket = io('https://airpong.herokuapp.com//');
+const socket = io('https://airpong.herokuapp.com/');
 
 socket.emit('play')
 socket.on('reverseplay', () => {
