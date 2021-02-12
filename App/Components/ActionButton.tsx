fe1c88d15ecda02966a0423428eb0181b335d3f6
valentingnt <valentin64.genest@gmail.com>
2021-02-12 12:39:15 +0100

    fixed layout buttons

--- App/Components/ActionButton.tsx
+++ App/Components/ActionButton.tsx
@@ -1,36 +1,35 @@
-import React from 'react';
-import { Pressable, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Pressable, Text, StyleSheet } from "react-native";
 
 interface Props {
-    text: string,
-    onPress?: () => void
+  text: string;
+  onPress?: () => void;
 }
 
 const ActionButton = ({ text, onPress }: Props) => {
-    return (
-        <Pressable
-            onPress={onPress}
-            style={({ pressed }) => [
-            {
-                backgroundColor: pressed ? "#FFA8A8" : "#efefef",
-            },
-            styles.actionbutton,
-            ]}
-        >
-            <Text style={{ color: "#000" }}>{ text }</Text>
-        </Pressable>
-    )
-}
+  return (
+    <Pressable
+      onPress={onPress}
+      style={({ pressed }) => [
+        {
+          backgroundColor: pressed ? "#FFA8A8" : "#efefef",
+        },
+        styles.actionbutton,
+      ]}
+    >
+      <Text style={{ color: "#000", fontFamily: "Arciform" }}>{text}</Text>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-    actionbutton: {
-      alignItems: "center",
-      justifyContent: "center",
-      height: 100,
-      width: 100,
-      borderRadius: 50,
-    }
+  actionbutton: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+  },
 });
-  
 
 export default ActionButton;
